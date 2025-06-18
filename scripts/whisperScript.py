49c476b169b09aa70e881a5df8d4014dc1775f46
mwMotionDesign <kontakt@michael-weidenfeld.de>
2025-06-18 14:30:18 +0200

    Created Venv Env... for Python and such.

--- scripts/whisperScript.py
+++ scripts/whisperScript.py
@@ -1,7 +1,19 @@
 import sys
 import whisper
+# import torchaudio as ta
+# import torch
 
-model = whisper.load_model("medium")  # "base", "small", "medium", "large"
+# # --- DETECT HARDWARE ---
+# if torch.cuda.is_available():
+#     device = "cuda"
+# elif torch.backends.mps.is_available():
+#     device = "mps"
+# else:
+#     device = "cpu"
+
+# print(f"Using device: {device}")
+
+model = whisper.load_model("base")  # "base", "small", "medium", "large"
 
 path = sys.argv[1]
 result = model.transcribe(path)
