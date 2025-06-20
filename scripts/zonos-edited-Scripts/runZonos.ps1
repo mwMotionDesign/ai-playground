442cf3e650d852fde31da6202ce6877fe8fb2426
mwMotionDesign <kontakt@michael-weidenfeld.de>
2025-06-20 19:23:51 +0200

    git Changes

---
+++ scripts/zonos-edited-Scripts/runZonos.ps1
@@ -0,0 +1,78 @@
+# $logFile = "$PSScriptRoot\run_gradio.log"
+# "[$(Get-Date -Format 'HH:mm:ss')] run_gradio.ps1 start" | Out-File -FilePath $logFile -Encoding utf8
+
+
+
+# ----- run script by @bdsqlsz, A-N-I-K
+
+# Find where the latest MSVC is installed dynamically
+$vswherePath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if (Test-Path $vswherePath) {
+  $vcvarsPath = & $vswherePath -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+  if ($vcvarsPath) {
+    $vcvarsPath = Join-Path $vcvarsPath "VC\Auxiliary\Build\vcvars64.bat"
+  }
+}
+else {
+  Write-Host "ERROR: vswhere.exe not found! Please ensure Visual Studio is installed."
+}
+
+# Ensure the path is valid
+if (Test-Path $vcvarsPath) {
+  Write-Host ""
+  Write-Host "Setting up MSVC environment..."
+  cmd.exe /c "`"$vcvarsPath`" && set" | foreach {
+    if ($_ -match "^(.*?)=(.*)$") {
+      Set-Content -Path "env:\$($matches[1])" -Value "$($matches[2])"
+    }
+  }
+}
+else {
+  Write-Host "ERROR: vcvars64.bat could not be found! Please make sure MSVC is installed properly."
+}
+
+# Activate python venv
+Set-Location $PSScriptRoot
+
+if ($env:OS -ilike "*windows*") {
+  if (Test-Path "./venv/Scripts/activate") {
+    Write-Output "Windows venv"
+    ./venv/Scripts/activate
+  }
+  elseif (Test-Path "./.venv/Scripts/activate") {
+    Write-Output "Windows .venv"
+    ./.venv/Scripts/activate
+  }
+}
+elseif (Test-Path "./venv/bin/activate") {
+  Write-Output "Linux venv"
+  ./venv/bin/Activate.ps1
+}
+elseif (Test-Path "./.venv/bin/activate") {
+  Write-Output "Linux .venv"
+  ./.venv/bin/activate.ps1
+}
+
+$Env:HF_HOME = $PSScriptRoot + "\huggingface"
+$Env:TORCH_HOME = $PSScriptRoot + "\torch"
+#$Env:HF_ENDPOINT = "https://hf-mirror.com"
+$Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
+$Env:CUDA_HOME = "${env:CUDA_PATH}"
+$Env:PHONEMIZER_ESPEAK_LIBRARY = "C:\Program Files\eSpeak NG\libespeak-ng.dll"
+$Env:GRADIO_HOST = "127.0.0.1"
+
+
+
+# --- END SCRIPT ---
+
+# "[$(Get-Date -Format 'HH:mm:ss')] run_gradio.ps1 end" | Out-File -FilePath $logFile -Encoding utf8 -Append
+
+Write-Output "Starting gradio_interface.py"
+
+python -m gradio_interface
+
+Write-Output "Done!"
+
+
+Read-Host | Out-Null ;
